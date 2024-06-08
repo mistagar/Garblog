5d10fd7d562833ae96d06fb6816c22a1f3ece17e
mistagar <gar.shalom1@gmail.com>
2024-06-08 16:21:51 +0100

    week 13 assignment

--- django_project/garblog/tests.py
+++ django_project/garblog/tests.py
@@ -32,13 +32,48 @@ class LoginViewTestCase(TestCase):
 class PostViewSetTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user = User.objects.create_user(username='john', password='P@ssword1')
         self.client.force_authenticate(user=self.user)
 
     def test_create_post(self):
         url = reverse('post-list')
-        data = {'title': 'Test Post', 'story': 'This is a test post.'}
+        data = {'title': 'My Blog', 'story': 'This is a test post.'}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 1)
-        self.assertEqual(Post.objects.get().title, 'Test Post')
+        self.assertEqual(Post.objects.get().title, 'My Blog')
+
+class BlogViewTestCase(TestCase):
+    def setUp(self):
+        Post.objects.create(title = 'Post 1', content = 'Test Content 1')
+        Post.objects.create(title = 'Post 2', content = 'Test content 2')
+    
+    def test_blog_list_view(self):
+        response = self.client.get(reverse('post-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertContains(response, 'Post 1')
+        self.assertContains(response, 'Post 2')
+
+class PostFormTestCase(TestCase):
+    def test_valid_post_form(self):
+        form_data = {'title':'Test Title', 'content':'Test Content'}
+        form = PostForm(data=form_data)
+        self.assertTrue(form.is_valid())
+    
+class UserPermissionsTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username = 'john', password = 'P@ssword1')
+        self.post = Post.objects.create(title = 'Test Post', content = 'Test content', author = self.user)
+
+        def test_post_author_access(self):
+            self.client.login(username = 'john', password = 'P@ssword1')
+            response = self.client.get('/blog/post/{}/edit/'.form(self.post.id))
+            self.assertEqual(response.status_code, 200)
+        
+        def test_non_author_access(self):
+            new_user = User.objects.create_user(username = 'doe', password = 'P@ssword1')
+            response = self.client.get('/blog/post/{}/edit/'.form(self.post.id))
+            self.assertEqual(response.status_code, 403)
+        
+    
--- django_project/garblog/urls.py
+++ django_project/garblog/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, PostViewSet, CommentViewSet, RegisterView, LoginView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = 'Blog Api',
+        default_version = 'v1',
+        description = 'API documentation for blog posts',
+        license = openapi.License(name = 'MIT License'),
+
+    ),
+    public = True,
+    permission_classes = (permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -11,4 +27,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('', include(router.urls)),
+    path('swagger/',schema_view.with_ui('swagger', cache_timeout = 0), name = 'schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
 ]
